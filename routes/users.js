8eb840b7c0a0f8baa76abecce222fb80342a3352
Tanisha Singh Chouhan <68784397+Tanish2000@users.noreply.github.com>
2022-07-18 16:26:45 +0530

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var url = "mongodb+srv://Tanish:tanish@cluster0.v1a3sps.mongodb.net/?retryWrites=true&w=majority/stuDB";
-mongoose.connect(url , ()=> console.log("DB connected"));
+var url = "mongodb+srv://Tanish:tanish@cluster0.v1a3sps.mongodb.net/mean_test?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Database connection successfull");
+}).catch((err) => {
+  console.log("Error: ", err)
+})
 var User = require("../model/userModel");
 
 /* Register */
